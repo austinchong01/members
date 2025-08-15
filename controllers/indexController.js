76f0155dddf56adcc9edd0363335ea1b3a8d6856
aweston <austin.inhyuk.chong@gmail.com>
2025-08-15 13:03:40 -0700

    Implement Basic Database

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,15 @@
-const db = require("../db/queries");
+// const db = require("../db/queries");
 
-async function getElements(req, res) {
-  try {
-    const people = await db.getElements();
-    res.render('index', { people: people });
-  } catch (error) {
-    console.error('Error fetching people:', error);
-    res.status(500).send('Error fetching data from database');
-  }
-}
+// async function getElements(req, res) {
+//   try {
+//     const people = await db.getElements();
+//     res.render('index', { people: people });
+//   } catch (error) {
+//     console.error('Error fetching people:', error);
+//     res.status(500).send('Error fetching data from database');
+//   }
+// }
 
-module.exports = {
-  getElements
-};
+// module.exports = {
+//   getElements
+// };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const indexController = require('../controllers/indexController.js');
+// const express = require('express');
+// const router = express.Router();
+// const indexController = require('../controllers/indexController.js');
 
-// Home page - show all inventory items
-router.get('/', indexController.getElements);
+// // Home page - show all inventory items
+// router.get('/', indexController.getElements);
 
-module.exports = router;
+// module.exports = router;
